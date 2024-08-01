bc526ae2acf329a97adbae7eeecf51230457e41d
ambrosayos <ambrosayos@mail.ru>
2024-08-01 18:02:11 +0300

    header title components

---
+++ components/shared/categories.tsx
@@ -0,0 +1,33 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+type Props = {
+  className?: string;
+};
+
+export const Categories: React.FC<Props> = ({ className }) => {
+  const categories = ["Все", "Суши", "Сеты", "Роллы", "Напитки", "Десерты"];
+  const activeIdx = 0;
+
+  return (
+    <div
+      className={cn("inline-flex gap-1 rounded-2xl bg-gray-50 p-2", className)}
+    >
+      {categories.map((category, idx) => {
+        return (
+          <a
+            href={"#" + category}
+            key={idx}
+            className={cn(
+              "flex h-11 items-center rounded-2xl px-5 font-bold text-zinc-500",
+              activeIdx === idx &&
+                "shadow-grey/30 bg-white text-zinc-900 shadow-md",
+            )}
+          >
+            <button>{category}</button>
+          </a>
+        );
+      })}
+    </div>
+  );
+};
---
+++ components/shared/header.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Image from "next/image";
+import { ArrowRight, Pizza, ShoppingCart, User } from "lucide-react";
+import Container from "./container";
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+
+type Props = {
+  className?: string;
+};
+
+const Header: React.FC<Props> = (className: Props) => {
+  return (
+    <>
+      <header className={cn("border border-b", className)}>
+        <Container className="flex items-center justify-between py-8">
+          <div className="flex items-center gap-4">
+            <Pizza size={32} />
+            <div className="">
+              <h1 className="text-center">PET PROJECT</h1>
+              <p className="text-center opacity-50">NextJS</p>
+            </div>
+          </div>
+
+          <div>
+            <input
+              type="text"
+              className="rounded border border-[rgba(0,0,0,0.4)]"
+            />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Button className="flex gap-2 rounded border hover:bg-neutral-300">
+              <User /> Войти
+            </Button>
+            <div className="group relative">
+              <Button className="rounded border hover:bg-neutral-300">
+                <b>520 P</b>
+                <span className="mx-3 h-full w-[1px] bg-black/30"></span>
+                <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                  <ShoppingCart size={16} strokeWidth={2} />
+                  <b>3</b>
+                </div>
+                <ArrowRight
+                  size={20}
+                  className="absolute right-5 -translate-x-2 opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
+                />
+              </Button>
+            </div>
+          </div>
+        </Container>
+      </header>
+    </>
+  );
+};
+export default Header;
